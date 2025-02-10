76beaae6c299416e5b9aa73966d7b19d316e50e8
T.N.SanthoshPriyan <santhoshpriyan2507@gmail.com>
2025-02-10 15:04:18 +0530

    Update vision.py

--- vision.py
+++ vision.py
@@ -5,7 +5,7 @@ import streamlit as st
 import os
 import google.generativeai as genai
 from PIL import Image
-
+import base64
 
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
@@ -18,10 +18,33 @@ def get_gemini_response(input, image):
         response = model.generate_content(image)
     return response.text
 
+def set_background(image_path):
+    with open(image_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode()
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/png;base64,{encoded_string});
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }}
+        .stHeader {{
+            padding: 0;
+            margin: 0;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
 st.set_page_config(page_title="Image Recognition App" , page_icon=":camera:", layout="centered", initial_sidebar_state="expanded")
 
-st.header("Image Recognition ChatBot")
+st.header("Image Recognition ChatBot", anchor=None)
 input = st.text_input("Input Prompt: ", key="input")
+
+set_background("d:/project/background_image.jpg") 
 
 uploaded_file = st.file_uploader("Choose an image... ", type=["jpg", "jpeg", "png"])
 image = ""
