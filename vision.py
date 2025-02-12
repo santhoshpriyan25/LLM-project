8c03e1b51571f7262de57d78c5fb12d4fc1ef858
T.N.SanthoshPriyan <santhoshpriyan2507@gmail.com>
2025-02-12 12:55:11 +0530

    Update vision.py

--- vision.py
+++ vision.py
@@ -1,11 +1,17 @@
+import streamlit as st  
+st.set_page_config(page_title="Image Recognition App" , page_icon=":camera:", layout="centered", initial_sidebar_state="expanded")
+
 from dotenv import load_dotenv
 load_dotenv()
 
-import streamlit as st  
 import os
 import google.generativeai as genai
 from PIL import Image
-
+import base64
+import threading
+import time
+from streamlit.runtime.scriptrunner import add_script_run_ctx
+import random
 
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
@@ -18,16 +24,55 @@ def get_gemini_response(input, image):
         response = model.generate_content(image)
     return response.text
 
-st.set_page_config(page_title="Image Recognition App" , page_icon=":camera:", layout="centered", initial_sidebar_state="expanded")
+def set_random_background(image_files):
+    image_file = random.choice(image_files)
+    with open(image_file, "rb") as f:
+        data = f.read()
+    encoded = base64.b64encode(data).decode()
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/png;base64,{encoded});
+            background-size: cover;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
-st.header("Image Recognition ChatBot")
-input = st.text_input("Input Prompt: ", key="input")
+# Set a random background image on refresh
+image_files = ["image 1.jpg", "image 2.jpg", "image 3.jpg"]
+set_random_background(image_files)
 
-uploaded_file = st.file_uploader("Choose an image... ", type=["jpg", "jpeg", "png"])
+st.markdown("<h1 style='font-style: italic;'>ChatBot with Image Recognition</h1>", unsafe_allow_html=True)
+input = st.text_input(" ChatBox: ", key="input")
+
+uploaded_file = st.file_uploader("Upload or Drag and Drop an image... ", type=["jpg", "jpeg", "png"])
 image = ""
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_container_width=True)
+
+st.markdown(
+    """
+    <style>
+    .stButton>button {
+        transition: all 0.3s ease;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        background-color: #28a745;
+        color: white;
+        border: 2px solid white;
+    }
+    .stButton>button:hover {
+        transform: scale(1.05);
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+        background-color: #218838;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 submit=st.button("Generate Response")
 
